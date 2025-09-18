d2f97ff742083fdae70b734c5ff6771fd260cea9
Samarth-Naik07 <151657695+Samarth-Naik07@users.noreply.github.com>
2025-09-19 01:43:46 +0530

    Fix CSS import casing for Netlify

--- ToDolist/src/assets/taskdetails.tsx
+++ ToDolist/src/assets/taskdetails.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import "./TaskDetails.css";
+import "./taskDetails.css";
 
 type Task = {
   _id: string;
