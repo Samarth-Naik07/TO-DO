99c917641392bb15c1ca22d5b194bc19fa8e1495
Samarth-Naik07 <151657695+Samarth-Naik07@users.noreply.github.com>
2025-09-19 00:49:52 +0530

    Update index.ts

--- todo-backend/src/index.ts
+++ todo-backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from "dotenv"; // ✅ import dotenv
+
 import taskRoutes from "./routes/taskRoutes.js";
 
 dotenv.config(); // ✅ load .env file
